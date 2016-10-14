95cfb311ffe016dba6f979b9043b37ed50070967
Phuc PNT <mail@phucpnt.com>
2016-10-14 23:34:40 +0700

    test (api jwt): complete test case for jwt enabled api

--- src/__test__/api-jwt.spec.js
+++ src/__test__/api-jwt.spec.js
@@ -21,6 +21,35 @@ function startKs(beforeStartKs = ks => ks, callback = () => {}) {
       'user model': 'User',
       'cookie secret': '(your secret here)'
     });
+
+    var Types = keystone.Field.Types;
+
+    var User = new keystone.List('User', {
+      // nodelete prevents people deleting the demo admin user
+      nodelete: true,
+    });
+
+    User.add({
+      name: { type: Types.Name, required: true, index: true },
+      email: { type: Types.Email, initial: true, required: true, index: true, unique: true },
+      phone: { type: String, width: 'short' },
+      password: { type: Types.Password, initial: true, required: false },
+    }, 'Permissions', {
+        isProtected: { type: Boolean, noedit: true, hidden: true },
+      });
+    User.register();
+    
+    new User.model({
+      name: {
+        first: 'Unit',
+        last: 'Test'
+      },
+      email: 'email@unittest.com',
+      password: 'test',
+      isAdmin: true,
+      isProtected: true,
+    }).save();
+        
     keystone = beforeStartKs(keystone);
     keystone.start({
       onStart: () => callback()
@@ -30,18 +59,49 @@ function startKs(beforeStartKs = ks => ks, callback = () => {}) {
 
 describe('Api exposed with JWT', () => {
   
-  beforeEach(done => {
+  before(done => {
     startKs(ks => {
       return enhanceKs(ks);
     }, done );
   });
   
-  afterEach(done => {
+  after(done => {
     keystone.httpServer.close(() => done());
   })
   
+  let signedToken = null;
   
-  it('should work', (done) => {
-    request(keystone.app).get('/keystone/api/users').expect(200, done);
-  })
+  it('should not allow query without JWT', (done) => {
+    request(keystone.app).get('/remote/api/users')
+      .expect(403)
+      .end((err, res) => {
+        console.log(res.body);
+        expect(res.body.error).to.be.defined;
+        done(err);
+      });
+  });
+  
+  it('should support sign in with email / password', done => {
+    request(keystone.app).post('/remote/signin')
+    .send({email: 'email@unittest.com', password: 'test'})
+    .expect(200)
+    .end((err, res) => {
+      expect(res.body.token).to.be.defined;
+      signedToken = res.body.token;
+      done(err);
+    });
+  });
+  
+  it('should support query with signed token', done => {
+    request(keystone.app).get('/remote/api/users')
+      .set({Authorization: `Bearer ${signedToken}`})
+      .expect(200)
+      .end((err, res) => {
+        console.log(res.body);
+        expect(res.body.results).to.exist;
+        expect(res.body.results).to.have.length.above(0);
+        done(err);
+      });
+  });
+  // todo: testing token expired?
 })
